bdef7e88055dddd96839383f6a24e891aafd9ed5
Sgit76 <shafiul.cse76@gmail.com>
2021-10-12 21:30:45 +0600

    added route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 
+import { BrowserRouter as Router ,Switch, Route} from 'react-router-dom';
 import './App.css';
 import Header from './component/Header/Header';
+import Inventory from './component/Inventory/Inventory';
+import NotFound from './component/NotFound/NotFound';
+import OrderReview from './component/OrderReview/OrderReview';
 import Shop from './component/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="*">
+              <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
